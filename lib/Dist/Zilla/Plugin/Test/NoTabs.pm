5ddb91bf7b90551d8f8c3164c358f8daba99c3d1
Karen Etheridge <ether@cpan.org>
2014-08-16 15:50:38 -0700

    add some keywords to metadata

--- lib/Dist/Zilla/Plugin/Test/NoTabs.pm
+++ lib/Dist/Zilla/Plugin/Test/NoTabs.pm
@@ -1,5 +1,6 @@
 package Dist::Zilla::Plugin::Test::NoTabs;
 # ABSTRACT: Release tests making sure hard tabs aren't used
+# KEYWORDS: plugin test testing whitespace tab formatting
 # vim: set ts=8 sw=4 tw=78 et :
 
 use Moose;
